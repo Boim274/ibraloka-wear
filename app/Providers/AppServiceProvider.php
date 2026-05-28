c78963af1bbabc29b47957a25fc5bfa318fe4963
ibrahim <155033257+Boim274@users.noreply.github.com>
2026-05-29 04:35:50 +0700

    update appservice dan nixpacks

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,29 +2,26 @@
 
 namespace App\Providers;
 
-use App\Models\Article;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Article;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        if (config('app.env') === 'local') {
-            $this->app['url']->forceRootUrl(request()->getSchemeAndHttpHost());
+        // 🔥 FORCE HTTPS (INI WAJIB)
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
         }
 
+        // Navbar data
         View::composer('components.navbar', function ($view) {
             $navbarCategories = Article::where('is_published', true)
                 ->select('category')
